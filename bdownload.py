2765c0b3c1b9a87fe648d3966ab499e53fc52e67
elpaca <elpaca2002@gmail.com>
2018-06-24 20:17:13 +0800

    batch downloading supported

--- bdownload.py
+++ bdownload.py
@@ -1,4 +1,5 @@
 from post import Post
+from part import Part
 import argparse
 import logging
 import os
@@ -22,22 +23,30 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
     ap = argparse.ArgumentParser(description='Bilibili Video Downloader')
-    ap.add_argument('avid', help='AVID of video to download.')
+    ap.add_argument('avid', nargs='+', help='AVID of video to download. Use space to seperate multiple avids.')
     ap.add_argument('-d', '--download', action='store_true', help='Download all parts of the video post. You should start the aria2c server manually before downloading.')
     ap.add_argument('-p', '--part', type=int, default=-1, help='Download specific part. Please input sequence number, e.g. 2 for the second part. ')
-    ap.add_argument('-c', '--cid-mode', action='store_true', help='Specify videos by cid rather than avid. Argument "avid" will be regarded as cid. You can use "," to seperate more videos.')
+    ap.add_argument('-c', '--cid-mode', action='store_true', help='Specify videos by cid rather than avid. Argument "avid" will be regarded as cid. Use space to seperate multiple cids.')
     args = ap.parse_args()
 
-    avid = args.avid
-    if avid.startswith('av'): avid = avid[2:]
-    post = Post(avid)
-    post.showInfo()
-
-    if args.download:
-        if args.part == -1:
-            logging.info('Downloading post: %s (%d parts in total)' % (post.title, len(post.parts)))
-            for p in post.parts:
+    if not args.cid_mode:
+        for avid in args.avid:
+            if avid.startswith('av'): avid = avid[2:]
+            logging.info('Downloading post av%s' % avid)
+            post = Post(avid)
+            post.showInfo()
+            if args.download:
+                if args.part == -1:
+                    logging.info('Downloading post: %s (%d parts in total)' % (post.title, len(post.parts)))
+                    for p in post.parts:
+                        p.download()
+                    logging.info('Download finished!')
+                else:
+                    post.parts[args.part - 1].download()
+    else:
+        if args.download:
+            for cid in args.avid:
+                p = Part(cid, str(cid), str(cid), None)
                 p.download()
-            logging.info('Download finished!')
-        else:
-            post.parts[args.part - 1].download()
+
+    
